68f4aacbbd9972e7006db0851625e4c58cef6cb1
phads091 <phads091@gmail.com>
2023-09-16 19:44:37 -0300

    Closes #1

--- src/main/java/br/com/truedev/ecommerce/EcommerceApplication.java
+++ src/main/java/br/com/truedev/ecommerce/EcommerceApplication.java
@@ -2,7 +2,6 @@ package br.com.truedev.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//teste de commit 
 @SpringBootApplication
 public class EcommerceApplication {
 
